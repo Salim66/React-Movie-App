436dcf4794cfc14195d2167d69896bfbff51befd
Salim66 <salimhasanriad8@gmail.com>
2022-07-18 20:25:39 +0600

    Work With State

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      movieName: "Fast X",
+    }
+  }
   render(){
     return (
       <div className="App">
-        <h1>Welcome To Salim</h1>
+        <h1>Welcome To ThereSixtyDegree</h1>
+        <p>My favorite movie is { this.state.movieName }</p>
+        <button onClick={ () => {
+          this.setState({ movieName: "Fast 11" });
+        } }>Fast 11</button>
+        <button onClick={ () => {
+          this.setState({ movieName: "Fast 9" });
+        } }>Fast 9</button>
       </div>
     );
   }
